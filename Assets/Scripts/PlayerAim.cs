0271123f9de45885a3b37f4f1974f8cd99724043
yongu2000 <yongu200@hawaii.edu>
2023-01-21 11:05:49 -1000

    Zoom Implement

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -7,9 +7,21 @@ public class PlayerAim : MonoBehaviour
 {
     public Cinemachine.AxisState xAxis, yAxis;
     [SerializeField] Transform camFollowPos;
+    [HideInInspector] public CinemachineVirtualCamera vCam;
+    public float adsFov = 40;
+    [HideInInspector] public float hipFov;
+    [HideInInspector] public float currentFov;
+    public float fovSmoothSpeed = 10;
+
+    PlayerAimBaseState currentState;
+    public PlayerHipFireState Hip = new PlayerHipFireState();
+    public PlayerAimState Aim = new PlayerAimState();
     // Start is called before the first frame update
     void Start()
     {
+        vCam = GetComponentInChildren<CinemachineVirtualCamera>();
+        hipFov = vCam.m_Lens.FieldOfView;
+        SwitchState(Hip);
 
     }
 
@@ -19,11 +31,19 @@ public class PlayerAim : MonoBehaviour
         xAxis.Update(Time.deltaTime);
         yAxis.Update(Time.deltaTime);
 
+        vCam.m_Lens.FieldOfView = Mathf.Lerp(vCam.m_Lens.FieldOfView, currentFov, fovSmoothSpeed * Time.deltaTime);
+        currentState.UpdateState(this);
     }
 
     private void LateUpdate()
     {
         camFollowPos.localEulerAngles = new Vector3(yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+    }
+
+    public void SwitchState(PlayerAimBaseState state)
+    {
+        currentState = state;
+        currentState.EnterState(this);
     }
 }
